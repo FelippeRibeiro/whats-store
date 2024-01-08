9eace10dedd83e9307b9199c22ce42741a3ee6c0
FelipeRibeiro <felipper43@gmail.com>
2024-01-08 17:57:20 -0300

    feat: create new methods of cart repository

--- src/repository/cart.repository.ts
+++ src/repository/cart.repository.ts
@@ -11,4 +11,23 @@ export class CartRepository implements ICartRepository {
   async getCartByUserId(userId: string): Promise<UserCart | null> {
     return this.DataBaseCart.findUnique({ where: { id: userId } });
   }
+  async getCartByUserAndProduct(userId: string, productId: number): Promise<UserCart | null> {
+    return this.DataBaseCart.findFirst({ where: { user: userId, productId } });
+  }
+
+  async updateQuantity(cartId: string, quantity: number): Promise<UserCart> {
+    return this.DataBaseCart.update({ where: { id: cartId }, data: { quantity } });
+  }
+
+  async deleteCart(userId: string, productId: number): Promise<UserCart> {
+    return this.DataBaseCart.delete({ where: { id: userId, productId } });
+  }
+
+  async createIfNotExistsOrUpdateQuantity(userId: string, productId: number, quantity: number): Promise<UserCart> {
+    const cart = await this.getCartByUserAndProduct(userId, productId);
+    if (cart) {
+      return this.updateQuantity(cart.id, quantity);
+    }
+    return this.createCart(userId, productId, quantity);
+  }
 }
--- src/repository/interfaces/cart.repository.interface.ts
+++ src/repository/interfaces/cart.repository.interface.ts
@@ -3,4 +3,8 @@ import { UserCart } from '@prisma/client';
 export interface ICartRepository {
   getCartByUserId(userId: string): Promise<UserCart | null>;
   createCart(userId: string, productId: number, quantity: number): Promise<UserCart>;
+  getCartByUserAndProduct(userId: string, productId: number): Promise<UserCart | null>;
+  updateQuantity(cartId: string, quantity: number): Promise<UserCart>;
+  deleteCart(userId: string, productId: number): Promise<UserCart>;
+  createIfNotExistsOrUpdateQuantity(userId: string, productId: number, quantity: number): Promise<UserCart>;
 }
